c1ee5052166cde5f171ceed8db592796228b7b26
Giusseppe <eppedelbugio@gmail.com>
2021-10-13 16:28:09 -0300

    countries, cities and interests migrations

---
+++ src/migrations/20190924230839-create-cities.ts
@@ -0,0 +1,56 @@
+import { QueryInterface } from 'sequelize';
+
+export const up = (queryInterface: QueryInterface, DataTypes: any) =>
+  queryInterface.createTable('Cities', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    photo: {
+      allowNull: true,
+      type: DataTypes.TEXT,
+    },
+    countryId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Countries',
+        key: 'id',
+      },
+    },
+    location: {
+      allowNull: false,
+      type: DataTypes.GEOMETRY,
+    },
+    latitude: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    longitude: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    isActive: {
+      allowNull: false,
+      defaultValue: true,
+      type: DataTypes.BOOLEAN,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  });
+
+export const down = (queryInterface: QueryInterface) =>
+  queryInterface.dropTable('Cities');
---
+++ src/migrations/20190924231049-create-interests.ts
@@ -0,0 +1,36 @@
+import { QueryInterface } from 'sequelize';
+
+export const up = (queryInterface: QueryInterface, DataTypes: any) =>
+  queryInterface.createTable('Interests', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      unique: true,
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    description: {
+      allowNull: true,
+      type: DataTypes.TEXT,
+    },
+    isActive: {
+      allowNull: false,
+      defaultValue: true,
+      type: DataTypes.BOOLEAN,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  });
+
+export const down = (queryInterface: QueryInterface) =>
+  queryInterface.dropTable('Interests');
